b739a72c2ec589d2d92ed25092669079d7d996a9
Jakner <35370137+Jakner@users.noreply.github.com>
2024-08-02 10:37:51 -0300

    update

--- src/Components/dialog/dialogadd.js
+++ src/Components/dialog/dialogadd.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
@@ -7,10 +7,8 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 import Axios from "axios";
 
-
-
 export default function FormDialogadd(props) {
-    const [editValues,setEditValues] = useState({
+    const [editValues, setEditValues] = useState({
         id: props.id,
         name: props.nome,
         data_nascimento: props.data_nascimento,
@@ -23,8 +21,8 @@ export default function FormDialogadd(props) {
         valor_mensalidade: props.valor_mensalidade,
     });
 
-   const handleAddAluno = async () => {
-        await Axios.post("https://server-mxrj.onrender.com/insert",{
+    const handleAddAluno = async () => {
+        await Axios.post("https://server-mxrj.onrender.com/insert", {
             id: editValues.id,
             nome: editValues.nome,
             data_nascimento: editValues.data_nascimento,
@@ -35,127 +33,146 @@ export default function FormDialogadd(props) {
             cpf: editValues.cpf,
             matricula: editValues.matricula,
             valor_mensalidade: editValues.valor_mensalidade,
-            
         });
         handleClose();
-        window.location.reload()
-   };     
+        window.location.reload();
+    };
 
+    const handleClose = () => {
+        props.setOpen(false);
+    };
 
-  const handleClose = () => {
-    props.setOpen(false);
-  };
+    const handleChangeValues = (e) => {
+        const { id, value } = e.target;
+        setEditValues(prevValues => ({
+            ...prevValues,
+            [id]: value,
+        }));
+    };
 
-  const handleChangeValues = value =>{
-    setEditValues(prevValues=>({
-        ...prevValues,
-        [value.target.id]: value.target.value,
-    }))
-  };
+    const handleCpfChange = (e) => {
+        const { value } = e.target;
+        const formattedCpf = value
+            .replace(/\D/g, '') // Remove caracteres não numéricos
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+        setEditValues(prevValues => ({
+            ...prevValues,
+            cpf: formattedCpf,
+        }));
+    };
 
-  return (
-    <Dialog open={props.open} onClose={handleClose}>
-      <DialogTitle>Cadastrar</DialogTitle>
-      <DialogContent>
-        <TextField
-          autoFocus
-          margin="dense"
-          id="nome"
-          label="Nome"
-          defaultValue={props.nome}
-          onChange={handleChangeValues}
-          type="text"
-          fullWidth
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="data_nascimento"
-          label="data de Nascimento"
-          defaultValue={props.data_nascimento}
-          onChange={handleChangeValues}
-          type="text"
-          fullWidth
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="email"
-          label="Email"
-          defaultValue={props.email}
-          onChange={handleChangeValues}
-          type="text"
-          fullWidth
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="telefone"
-          label="Telefone"
-          defaultValue={props.telefone}
-          onChange={handleChangeValues}
-          type="text"
-          fullWidth
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="endereco"
-          label="Endereco"
-          defaultValue={props.endereco}
-          onChange={handleChangeValues}
-          type="text"
-          fullWidth
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="rg"
-          label="RG"
-          defaultValue={props.rg}
-          onChange={handleChangeValues}
-          type="text"
-          fullWidth
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="cpf"
-          label="CPF"
-          defaultValue={props.cpf}
-          onChange={handleChangeValues}
-          type="text"
-          fullWidth
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="matricula"
-          label="Matricula"
-          defaultValue={props.matricula}
-          onChange={handleChangeValues}
-          type="text"
-          fullWidth
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="valor_mensalidade"
-          label="Valor Mensalidade"
-          defaultValue={props.valor_mensalidade}
-          onChange={handleChangeValues}
-          type="text"
-          fullWidth
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={()=>{
-          handleClose()
-        }}>Cancel</Button>
-        <Button onClick={()=>{
-          handleAddAluno(); 
-        }}>Salvar</Button>
-      </DialogActions>
-    </Dialog>
-  );
+    const handleValorChange = (e) => {
+        const { value } = e.target;
+        // Permite apenas números e faz a formatação se necessário
+        setEditValues(prevValues => ({
+            ...prevValues,
+            valor_mensalidade: value.replace(/[^0-9]/g, ''),
+        }));
+    };
+
+    return (
+        <Dialog open={props.open} onClose={handleClose}>
+            <DialogTitle>Cadastrar</DialogTitle>
+            <DialogContent>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="nome"
+                    label="Nome"
+                    value={editValues.name}
+                    onChange={handleChangeValues}
+                    type="text"
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="data_nascimento"
+                    label="Data de Nascimento"
+                    value={editValues.data_nascimento}
+                    onChange={handleChangeValues}
+                    type="text"
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="email"
+                    label="Email"
+                    value={editValues.email}
+                    onChange={handleChangeValues}
+                    type="text"
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="telefone"
+                    label="Telefone"
+                    value={editValues.telefone}
+                    onChange={handleChangeValues}
+                    type="text"
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="endereco"
+                    label="Endereço"
+                    value={editValues.endereco}
+                    onChange={handleChangeValues}
+                    type="text"
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="rg"
+                    label="RG"
+                    value={editValues.rg}
+                    onChange={handleChangeValues}
+                    type="text"
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="cpf"
+                    label="CPF"
+                    value={editValues.cpf}
+                    onChange={handleCpfChange}
+                    type="text"
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="matricula"
+                    label="Matrícula"
+                    value={editValues.matricula}
+                    onChange={handleChangeValues}
+                    type="text"
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="valor_mensalidade"
+                    label="Valor Mensalidade"
+                    value={editValues.valor_mensalidade}
+                    onChange={handleValorChange}
+                    type="text"
+                    inputMode="numeric" // Exibe teclado numérico em dispositivos móveis
+                    pattern="[0-9]*" // Validação de números apenas
+                    fullWidth
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleAddAluno}>Salvar</Button>
+            </DialogActions>
+        </Dialog>
+    );
 }
